78ca3a94fb74a23929c7407ed98f96776acf9681
Carloslt <carloslt.proyectos@gmail.com>
2023-10-14 21:05:18 +0200

    view payments

--- client/src/routes/payments/+page.svelte
+++ client/src/routes/payments/+page.svelte
@@ -1,4 +1,43 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	type PaymentData = {
+		amount: string;
+		client_id: number;
+		email: string;
+		id: number;
+		lastlogin: string;
+		name: string;
+		paid_date: string;
+		paid_status: boolean;
+		status: boolean;
+	};
+
+	let payments: PaymentData[] = [];
+
+	onMount(async () => {
+		try {
+			const storedToken = localStorage.getItem('token');
+			const response = await fetch('http://localhost:5005/api/payments', {
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${storedToken}`,
+					'Content-Type': 'application/json'
+				}
+			});
+			const result = await response.json();
+			payments = result;
+			console.log(payments);
+		} catch (error) {
+			console.log(error);
+		}
+	});
 </script>
 
 <h1>payments</h1>
+<p>nnombre</p>
+<ul>
+	{#each payments as payment (payment.id)}
+		<li>{payment.name}</li>
+	{/each}
+</ul>
